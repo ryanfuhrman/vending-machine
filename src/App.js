189e1fa0bd8e76591ee9039b38d3ef2c46810d2f
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-09-06 15:46:46 -0400

    Set up react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import VendingMachine from "./VendingMachine";
+import Coke from "./Coke";
+import Snickers from "./Snickers";
+import Doritos from "./Doritos";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" component={VendingMachine} />
+      <Route exact path="/coke" component={Coke} />
+      <Route exact path="/snickers" component={Snickers} />
+      <Route exact path="/doritos" component={Doritos} />
     </div>
   );
 }
